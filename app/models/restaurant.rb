909efba077088f64556988062ea510af292eb823
aschemily <aschemily@gmail.com>
2018-11-28 11:11:19 -0500

    code challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,31 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
   def initialize(name)
     @name = name
+
+
+    @@all << self
   end
+
+
+  def self.find_by_name(name)
+    self.all.find do |restaurant|
+      restaurant.name #==  self
+      end
+    end
+
+    def customers
+      Review.all.map do |review|
+        review.customer
+      end
+    end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,26 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#customer :first_name, :last_name
+c1 = Customer.new("Emily", "Asch")
+c2 = Customer.new("Hayley", "Shear")
+c3 = Customer.new("Olivia", "Radick")
+c4 = Customer.new("Emily", "Smith")
+
+
+#restaurant
+r1 = Restaurant.new("Jin Ramen")
+r2 = Restaurant.new("Chipotle")
+r3 = Restaurant.new("McDonalds")
+
+#rating (customer, restaurant, rating, content)
+rt1 = Review.new(c1,r1,5,"Love")
+rt2 = Review.new(c1,r2,3, "amazing")
+rt3 = Review.new(c2,r3,2, "gross")
+rt4 = Review.new(c3,r3,2, "gross")
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
