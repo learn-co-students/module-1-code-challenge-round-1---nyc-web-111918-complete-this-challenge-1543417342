865a13178ddb782fdd1d0c36c66d59d209597d8f
satyasinha94 <satyasinha94@gmail.com>
2018-11-29 09:47:22 -0500

    Redo

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,10 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+c1 = Customer.new("bob", "smith")
+c2 = Customer.new("bob", "smyth")
 
+r1 = Restaurant.new("chilis")
+r2 = Restaurant.new("the tang")
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
